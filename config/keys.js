3c1fd6954bdebb0005c5fb25f15ec7ea9976e05f
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-06-27 14:47:12 -0400

    connected to mongo db

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://Jon:123456jon@ds121341.mlab.com:21341/mearn_shoping'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+// bodyParser Middle ware
+
+app.use(bodyParser.json());
+
+// DB Config 
+
+const db = require('./config/keys').mongoURI
+
+// connect to mongo 
+
+mongoose
+  .connect(db)
+  .then(() => console.log('Mongo Connected yay!!'))
+  .catch(err => console.log(err));
+  
+  
+  const port = process.env.PORT || 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`))
